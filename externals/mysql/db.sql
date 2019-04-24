c00720a29391d96db4838028efc167cf607876a0
ilhammhdd <milham939@gmail.com>
2019-04-25 04:00:14 +0700

    done db schema, mysql and kafka externals

--- externals/mysql/db.sql
+++ externals/mysql/db.sql
@@ -13,6 +13,28 @@ CREATE TABLE IF NOT EXISTS storefronts(
     `user_uuid` VARCHAR(64)
 );
 
+CREATE TABLE IF NOT EXISTS brands(
+    `id` BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `uuid` VARCHAR(64) UNIQUE,
+    `name` VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS items(
+    `id` BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `uuid` VARCHAR(64) UNIQUE,
+    `brand_uuid` VARCHAR(64),
+    `name` VARCHAR(255),
+    `amount` INT(20),
+    `unit` VARCHAR(255),
+    `price` INT(20),
+    `description` TEXT,
+    `photo` VARCHAR(255),
+
+    FOREIGN KEY(brand_uuid)
+    REFERENCES brands(uuid)
+    ON DELETE CASCADE
+);
+
 CREATE TABLE IF NOT EXISTS item_storefronts(
     `id` BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `uuid` VARCHAR(64) UNIQUE,
@@ -21,6 +43,8 @@ CREATE TABLE IF NOT EXISTS item_storefronts(
 
     FOREIGN KEY(storefront_uuid)
     REFERENCES storefronts(uuid)
+    ON DELETE CASCADE,
+    FOREIGN KEY(item_uuid)
+    REFERENCES items(uuid)
+    ON DELETE CASCADE
 );
-
--- mamak japar : 085880692628
